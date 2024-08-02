cffe21df8a7aba9f7075e148156308a730a9f8b6
fdigrego <fabian.digregorio@gmail.com>
2024-08-01 21:10:16 -0300

    start c/forms

--- entrega3/AppCoder/urls.py
+++ entrega3/AppCoder/urls.py
@@ -6,5 +6,13 @@ urlpatterns = [
     path('cursos/', views.cursos, name='cursos'),
     path('profesores/', views.profesores, name='profesores'),
     path('estudiantes/', views.estudiantes, name='estudiantes'),
-    path('entregables/', views.entregables, name='entregables')
+    path('entregables/', views.entregables, name='entregables'),
+    
+    path('cursoFormulario/', views.cursoFormulario, name='cursoFormulario'),
+    path('estudianteFormulario/', views.estudianteFormulario, name='estudianteFormulario'),
+    path('profesorFormulario/', views.profesorFormulario, name='profesorFormulario'),
+    path('entregableFormulario/', views.entregableFormulario, name='entregableFormulario'),
+    
+    #path('busquedaComision', views.busquedaComision, name='busquedaComision'),
+    #path('buscar', views.buscar),
 ]
--- entrega3/AppCoder/views.py
+++ entrega3/AppCoder/views.py
@@ -1,19 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from AppCoder.models import Curso, Profesor, Estudiante, Entregable
+from AppCoder.forms import CursoFormulario
 
 # Create your views here.
 
 def inicio(request):
     return render(request, "AppCoder/index.html")
-
 def cursos(request):
     return render(request, "AppCoder/cursos.html")
-
 def profesores(request):
     return render(request, "AppCoder/profesores.html")
-
 def estudiantes(request):
     return render(request, "AppCoder/estudiantes.html")
-
 def entregables(request):
     return render(request, "AppCoder/entregables.html")
+
+def cursoFormulario(request):
+    
+    if request.method == "POST":
+        miFormulario = CursoFormulario(request.POST)
+        print(miFormulario)
+        
+        if miFormulario.is_valid():
+            informacion = miFormulario.cleaned_data
+            curso = Curso(nombre=informacion['curso'], camada=informacion['camada'])
+            curso.save()
+            return render(request, "AppCoder/cursoFormulario.html", {"miFormulario": miFormulario})
+            #return redirect('cursos')
+
+    else:
+        miFormulario = CursoFormulario()
+
+    return render(request, "AppCoder/cursoFormulario.html", {"miFormulario": miFormulario})
+
+def estudianteFormulario(request):
+    return render(request, "AppCoder/estudianteFormulario.html")
+def profesorFormulario(request):
+    return render(request, "AppCoder/profesorFormulario.html")
+def entregableFormulario(request):
+    return render(request, "AppCoder/entregableFormulario.html")
